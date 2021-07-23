dc83e4c7b36615e3f8538dd7184dab12aa79d79d
everly <everlyprecia@gmail.com>
2021-07-23 10:12:32 +0530

    formatting code

--- src/script.js
+++ src/script.js
@@ -5,3 +5,16 @@ window.onload=function(){
         nlinks.classList.toggle('act');
     })
 }
+window.addEventListener('scroll',()=>{
+    let anime=document.querySelectorAll('.reveal');
+    let size=window.innerHeight;
+    for(var i=0; i<anime.length;i++){
+        var top=anime[i].getBoundingClientRect().top;
+        if(top<size){
+            anime[i].classList.add('active');
+            
+        }
+        
+    } 
+ 
+});
